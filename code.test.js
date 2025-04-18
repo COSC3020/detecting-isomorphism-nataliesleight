0ec7ffd1939e8dcf6723bd148d712c9567c89c64
nataliesleight <nsleight@uwyo.edu>
2025-04-18 15:01:02 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -17,5 +17,45 @@ var testGraph2 = [  // 4 nodes, isomorphic to testGraph1
     [1,1,0,0]
 ];
 
-var result = hasCycle(testGraph1, testGraph2);
+var testGraph3 = [ // 4 nodoes, isomorphic to testGraph4
+    [0,0,0,1],
+    [0,0,0,1],
+    [0,0,0,1],
+    [1,1,1,0]
+];
+
+var testGraph4 = [ // 4 nodes, isomorphic to testGraph3
+    [0,1,0,0],
+    [1,0,1,1],
+    [0,1,0,0],
+    [0,1,0,0]
+]
+
+var testGraph5 = [ // 3 nodes
+    [0,0,1],
+    [0,0,1],
+    [1,1,0]
+];
+
+var testGraph6 = [ // 4 nodes, same degrees as testGraph1 and testGraph2 but NOT isomorphic
+    [0,1,1,1],
+    [1,0,1,1],
+    [1,1,0,0],
+    [1,0,0,1]
+];
+
+var result = are_isomorphic(testGraph1, testGraph2);
 jsc.assert(JSON.stringify(result) == JSON.stringify(true));
+
+
+var result = are_isomorphic(testGraph3, testGraph4);
+jsc.assert(JSON.stringify(result) == JSON.stringify(true));
+
+var result = are_isomorphic(testGraph1, testGraph4);
+jsc.assert(JSON.stringify(result) == JSON.stringify(false));
+
+var result = are_isomorphic(testGraph1, testGraph6);
+jsc.assert(JSON.stringify(result) == JSON.stringify(false));
+
+var result = are_isomorphic(testGrap2, testGraph5);
+jsc.assert(JSON.stringify(result) == JSON.stringify(false));
